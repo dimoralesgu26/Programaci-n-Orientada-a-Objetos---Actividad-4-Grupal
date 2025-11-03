dcfd032b8142a9170c0d5351e5c9d53cf13b5b00
dimoralesgu26 <dimoralesgu@unal.edu.co>
2025-11-03 14:18:37 -0500

    Principal.java

--- ejerciciopropuesto68/Principal.java
+++ ejerciciopropuesto68/Principal.java
@@ -125,6 +125,7 @@ public class Principal extends javax.swing.JFrame {
 
     private void limpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_limpiarActionPerformed
         txtTexto.setText(" ");
+        txtError.setText(" ");
     }//GEN-LAST:event_limpiarActionPerformed
 
     /**
